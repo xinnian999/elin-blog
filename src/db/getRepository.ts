d885321fd52c4518afd554768c43380e3332d670
v_huyilin <v_huyilin@baidu.com>
2025-01-26 11:10:19 +0800

    feat docker

--- src/db/getRepository.ts
+++ src/db/getRepository.ts
@@ -6,14 +6,15 @@ let isInitialized = false;
 
 const AppDataSource = new DataSource({
   type: "mysql",
-  host: process.env.DB_HOST,  // 使用环境变量来设置 host
-  port: parseInt(process.env.DB_PORT || '3306'),  // 使用环境变量来设置端口
+  host: process.env.DB_HOST, // 使用环境变量来设置 host
+  port: parseInt(process.env.DB_PORT || "3306"), // 使用环境变量来设置端口
   username: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   entities: Entities,
-  synchronize: true, 
+  synchronize: true,
   logging: false,
+  charset: "utf8mb4",
 });
 
 export const getRepository = async <T extends ObjectLiteral>(
