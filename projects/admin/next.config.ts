574cdf0a90b093d5bf9c4e5b1da065fd4f1c532f
hyl <3307378337@qq.com>
2025-02-07 21:57:47 +0800

    feat docker

--- projects/admin/next.config.ts
+++ projects/admin/next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  distDir: process.env.NODE_ENV === 'production' ? 'dist/build' : 'dist/dev',
+  distDir: process.env.NODE_ENV === 'production' ? 'dist/build' : 'dist/dev'
 };
 
 export default nextConfig;
